9d4650292e7533d5f8dc83435f723389ded14fea
thanh nhat <1612390@hcmut.edu.vn>
2017-11-06 21:56:51 +0700

    zombie

---
+++ Assets/Scripts/EndWay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndWay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerExit2D(Collider2D EndWay)
+    {
+        if(EndWay.gameObject.tag =="Ground")
+        {
+            enemyRun.speed *= -1;
+        }
+    }
+}
---
+++ Assets/Scripts/enemyRun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyRun : MonoBehaviour {
+    Rigidbody2D rbEnemy;
+    public static float speed=15;
+	// Use this for initialization
+	void Start () {
+        rbEnemy = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rbEnemy.velocity = new Vector2(-speed, rbEnemy.velocity.y);
+	}
+}
