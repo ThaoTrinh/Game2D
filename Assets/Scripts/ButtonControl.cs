4adf8a8ac128fe7732ce4543db3bb25bf1d6c51c
ThaoTrinh <trinhthithuthao>
2017-11-19 13:17:29 +0700

    guide

--- Assets/Scripts/ButtonControl.cs
+++ Assets/Scripts/ButtonControl.cs
@@ -1,11 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
+
 
 public class ButtonControl : MonoBehaviour {
 
+ 
+	public static string str;
+	public static string filepath;
 
+	public static string GetUserPath() 
+	{
+		return Environment
+			.GetFolderPath(
+			   Environment.SpecialFolder.MyDocuments);
+	}
+
+
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+	void Start () {
+			string folderpath = GetUserPath() + "/Strong Girl";
+
+		
+			if (!Directory.Exists(folderpath))
+			{
+				Directory.CreateDirectory(folderpath);
+			}
+
+		 filepath = folderpath + "/Loadlevel.txt";
+		if (!File.Exists(filepath))
+		{
+
+			using (FileStream fs = File.Create(filepath))
+			{
+				Byte[] info = new UTF8Encoding(true)
+						 .GetBytes("1");
+				fs.Write(info, 0, info.Length);
+			}
+		}
+
+			using (StreamReader sr = File.OpenText(filepath))
+			{
+				 str = sr.ReadLine();
+			
+			}
+
+	}
 	public ButtonControl.ButtonType bt;
 
 	public ButtonControl(){}
@@ -15,105 +58,94 @@ public class ButtonControl : MonoBehaviour {
 		transform.localScale = new Vector3(250f, 250f);
 
 	}
+	
 
 	private void OnMouseUp()
 	{
-		//transform.localScale = new Vector3(0.5f,0.5f);
-		string str ;
-		
+
 		if(bt==ButtonControl.ButtonType.btPlay){
 			Application.LoadLevel("LevelScreen");
-			//return;
+		
 		}
 
 		  if (bt == ButtonControl.ButtonType.btContinue)
         {
             Application.LoadLevel("mainLevel");
-            //	return;
+            
+        }
+
+		if (bt == ButtonControl.ButtonType.btGuide)
+        {
+            Application.LoadLevel("guideScreen");
+            
         }
 
 		if(bt==ButtonControl.ButtonType.btLevel1){
 			Application.LoadLevel("Level1");
-		//	return;
-		}
-
-		using (StreamReader sr = new StreamReader("Loadlevel.txt")){
-			str = sr.ReadLine();
+	
 		}
 
 		int level = int.Parse(str);
 		if(bt==ButtonControl.ButtonType.btLevel2){
 			if(level > 1)
 			Application.LoadLevel("Level2");
-			//return;
-		//	return;
 		}
 
 		if(bt==ButtonControl.ButtonType.btLevel3){
 			if(level > 2)
 			Application.LoadLevel("Level3");
-			//return;
-		//	return;
 		}
 
-		if(bt==ButtonControl.ButtonType.btLevel3){
+		if(bt==ButtonControl.ButtonType.btLevel4){
 			if(level > 3)
 			Application.LoadLevel("Level4");
-			//return;
-		//	return;
 		}
       
 
 		if (bt == ButtonControl.ButtonType.Door)
         {
             Application.LoadLevel("Level2");
-			using (StreamWriter sw = new StreamWriter("Loadlevel.txt")){
-				string data = "2";
-				sw.WriteLine(data);
+			if(level<2){
+				using (StreamWriter sw = new StreamWriter(filepath)){
+					string data = "2";
+					sw.WriteLine(data);
+				}
 			}
-            //	return;
+			
         }
 
 		if (bt == ButtonControl.ButtonType.Door2)
         {
             Application.LoadLevel("Level3");
-			using (StreamWriter sw = new StreamWriter("Loadlevel.txt")){
+			if(level<3){
+			using (StreamWriter sw = new StreamWriter(filepath)){
 				string data = "3";
 				sw.WriteLine(data);
 			}
-            //	return;
+			}
         }
 
 		if (bt == ButtonControl.ButtonType.Door3)
         {
             Application.LoadLevel("Level4");
-			using (StreamWriter sw = new StreamWriter("Loadlevel.txt")){
+			if(level<4){
+			using (StreamWriter sw = new StreamWriter(filepath)){
 				string data = "4";
 				sw.WriteLine(data);
 			}
-            //	return;
+			}
         }
 
 		if (bt == ButtonControl.ButtonType.khobau)
         {
             Application.LoadLevel("winner");
-			
-            //	return;
         }
 
 		if (bt == ButtonControl.ButtonType.exit)
         {
             Application.Quit();
-			
-            //	return;
         }
     }
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		
 	}
@@ -129,6 +161,7 @@ public class ButtonControl : MonoBehaviour {
 		Door2,
 		Door3,
 		khobau,
-		exit
+		exit,
+		btGuide
 	}
 }
